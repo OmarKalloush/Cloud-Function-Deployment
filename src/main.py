98777fba4cb82db317c9ae3a3846ceaf91182ab4
OmarKalloush <omar.kalloush@optimumpartners.co>
2022-03-15 10:58:48 +0300

    Test

--- src/main.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-
-def pipelines_trigger(event, context):
-    if 'conversion' in event['name']:
-        file_name = event['name']
-        bucket_name = event['bucket']
-        print(f"gs:\\{bucket_name}\{file_name}")
